c2c7d6eedd02137087f20a42ce214d02b816e9ef
adrianadhari <adrianadhari@protonmail.com>
2024-01-19 15:52:32 +0700

    First Commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        bgPrimary: "#090909",
+        textPrimary: "#FFD600",
+        textSecondary: "#707070"
+      }
+    },
+  },
+  plugins: [],
+}
+
